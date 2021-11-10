1df6022de6301be77e749c210ee9d6a84c81ba42
rizwan-858 <rizwanpathan858@gmail.com>
2021-11-10 11:12:00 +0000

    Lfinnovations-task

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,14 +1,16 @@
 import {Component} from 'react'
 
 import {Link} from 'react-router-dom'
-import {IoIosArrowBack, IoIosArrowForward} from 'react-icons/io'
+
+import Pagination from '@mui/material/Pagination'
+
 import './index.css'
 
 class Home extends Component {
   state = {
     dataList: [],
     limit: 10,
-    activePage: 1,
+    page: 1,
   }
 
   componentDidMount() {
@@ -16,8 +18,9 @@ class Home extends Component {
   }
 
   getPokemonData = async () => {
-    const {activePage, limit} = this.state
-    const offset = (activePage - 1) * limit
+    const {page, limit} = this.state
+    console.log(page)
+    const offset = (page - 1) * limit
     const apiUrl = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`
     const options = {
       method: 'GET',
@@ -51,63 +54,22 @@ class Home extends Component {
     )
   }
 
-  onForward = () => {
-    const {activePage} = this.state
-    if (activePage < 10) {
-      this.setState(
-        prevState => ({
-          activePage: prevState.activePage + 1,
-        }),
-        this.getPokemonData,
-      )
-    }
-  }
-
-  onBackward = () => {
-    const {activePage} = this.state
-    if (activePage > 1) {
-      this.setState(
-        prevState => ({
-          activePage: prevState.activePage - 1,
-        }),
-        this.getPokemonData,
-      )
-    }
-  }
-
   render() {
-    const {activePage} = this.state
+    const {page} = this.state
+    console.log(page)
     return (
       <div className="home-container">
         <h1 className="heading">Pokemons</h1>
         {this.renderPokemonsList()}
-        <div className="pagination-container">
-          <div className="btn-container">
-            <button
-              className="btn"
-              testid="pagination-left-button"
-              onClick={this.onBackward}
-              type="button"
-            >
-              <IoIosArrowBack />
-            </button>{' '}
-          </div>
-          <div className="pages">
-            <p testid="active-page-number">{activePage}</p>{' '}
-            <p className="text"> of </p>
-            <p>10</p>
-          </div>
-          <div className="btn-container">
-            <button
-              className="btn"
-              testid="pagination-right-button"
-              onClick={this.onForward}
-              type="button"
-            >
-              <IoIosArrowForward />
-            </button>
-          </div>
-        </div>
+        <Pagination
+          count={10}
+          color="primary"
+          page={page}
+          onChange={(event, value) => {
+            this.setState({page: value})
+            this.getPokemonData()
+          }}
+        />
       </div>
     )
   }
